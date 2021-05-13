681dc91f216ae0c693c4f7e3e3eed43f3bcc46ad
ssxvi <74996962+ssxvi@users.noreply.github.com>
2021-05-13 15:13:21 -0700

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,29 @@
+var grid = [];
+var y = [];
+var x = [];
+
+function setGrid(){
+
+
+    return tGrid
+}
+
+var john = land(12,2);
+//constructor
+console.log(asd);
+
+let land = class{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    get number(){
+
+    }
+
+    setVisible(){
+
+    }
+
+}
